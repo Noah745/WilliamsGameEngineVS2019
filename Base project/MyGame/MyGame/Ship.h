579ca32d36b59361187ed0d737bc84e82bfb1584
Noah Massengill <massengilln@rsd407.org>
2021-04-22 09:04:17 -0700

    laser and ship png

--- Base project/MyGame/MyGame/Ship.h
+++ Base project/MyGame/MyGame/Ship.h
@@ -10,7 +10,9 @@ public:
 
 	// Funtions overridden from GameObject:
 	void draw();
+	void update(sf::Time& elapsed);
 private:
 	sf::Sprite sprite_;
+	int fireTimer_ = 0
 };
 typedef std::shared_ptr<Ship> ShipPtr;
